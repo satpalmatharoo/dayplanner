58bc66cc9fc9e1b3618acc5d0a9ce5410d54ad7d
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-02 23:57:36 +0100

    delete obsolete functions

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
-// Add current day to top of calendar
-//Excerise 26
+// Add current day to top of calendar /Excerise 26
 
-function greet (){
-   console.log ("hello world");
-}
+var currentDay =$("#currentDay");
+var today = moment ();
+currentDay.text(today.format("dddd, MMMM Do YYYY"));
 
-greet ();
-
-const currentDayEl =$("#currentDay");
-console.log(); currentDayEl
-const now = moment ();
-currentDayEl.text(now.format("dddd, MMMM Do"));
-
-// var currentDay = $("#currentDay");
-// moment().format("dddd, MMMM Do YYYY")
-
-
-// var today = moment();
-// $("#currentDay").text(today.format("dddd, MMMM Do YYYY")
-
-// $("#1a").text(gradDate);
+// Timeblock colour code
 
 
 
-// var current = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-// var yyyy = today.getFullYear();
-
-// today = mm + '/' + dd + '/' + yyyy;
-// document.write(today);
-
-// var toDoList
 
 
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
