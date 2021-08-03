3143069fc57a202d15920328b1ae316b0e786e52
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-03 22:56:23 +0100

    Amend if statement - timeblock not working

--- script.js
+++ script.js
@@ -7,38 +7,33 @@ var currentHour = today.hours ()
 console.log (currentHour)
 
 
-//2 Timeblock colour code=textarea?
-    // a)add in function to find current hour?? Sources (Tech Admin)
+//2 Timeblock colour code
+    // a)add in function to find current hour
     // b)if statement
   
   for (let i = 9; i <18; i++){
      if 
    (currentHour == i) {
-    $(`#${i}`).addClass("present")
-
+    $(`#${i}`).addClass("future")
 
    }else if (currentHour < i) {
     $(`#${i}`).addClass("past")
 
    }else{
-    $(`#${i}`).addClass("future")
+    $(`#${i}`).addClass("present")
 
   }
   }
     
+// Add Local Storage
+localStorage.setItem("timeblock", "event");
 
 
-//3 Add local storage
-
-
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // WHEN I click into a timeblock
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
+
+
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
